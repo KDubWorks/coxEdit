963b48d619cd7ac1ac978a49b24d300af223f975
kenstjonweeks <kennethweeks@Kenneths-MacBook-Pro.local>
2017-09-10 14:32:23 -0700

    changes

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var win = $(window).width();
+var winH = $(window).height();
+var tA = $("#text-area").height();
+
+var remain = ((winH - tA) / 2) - 35;
+
+if(win >= 769 && win <=  1440) {
+
+	$(window).on("load", function() {
+
+		$("#text-area").css("top", remain + "px");
+		$("#text-area-icon").attr("src", "img/icons/powerOnDisplay.png");
+
+	});
+
+}
